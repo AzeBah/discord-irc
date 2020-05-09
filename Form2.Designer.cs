84b36524493afdd70b1cb90c7d2ce6495d3c92a6
AzeBah <63929044+AzeBah@users.noreply.github.com>
2020-05-09 18:19:49 -0400

    Add files via upload

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -35,9 +35,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(976, 441);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form2";
+            this.Opacity = 0.95D;
             this.Text = "Discord";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.Shown += new System.EventHandler(this.Form2_Load);
