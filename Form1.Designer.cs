7993b2ce5fd6710a373393a2737ffb43d5c3ad78
AzeBah <63929044+AzeBah@users.noreply.github.com>
2020-05-12 20:01:35 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTextBox.Location = new System.Drawing.Point(371, 187);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(215, 26);
+            this.emailTextBox.TabIndex = 1;
+            this.emailTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTextBox.Location = new System.Drawing.Point(371, 219);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(215, 26);
+            this.passwordTextBox.TabIndex = 2;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(430, 264);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(103, 38);
+            this.loginBtn.TabIndex = 3;
+            this.loginBtn.Text = "Log in";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.Text = "Discord";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginBtn;
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        private string messageToSend;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(54, 57, 63);
+            if (DiscordLogging.isLogged == false)
+            {
+                this.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void watchBtn_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    DiscordChannel channel = new DiscordChannel(channelIdTextBox.Text);
+                    if (InvokeRequired)
+                    {
+                        Invoke(new Action(CheckMessages));
+                    }
+                    else
+                    {
+                        BeginInvoke(new Action(CheckMessages));
+                    }
+                    Thread.Sleep(300);
+                }
+            });
+        }
+
+        private void channelMsgsTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // check every 5 seconds for new messages & place them in the textbox
+        private void CheckMessages()
+        {
+            channelMsgsTextBox.Text = "";
+            for (int i = DiscordChannel.messages.Count -1; i >= 0; i--)
+            {
+                channelMsgsTextBox.Text += DiscordChannel.messages[i].author.username + " : " + DiscordChannel.messages[i].content + Environment.NewLine;
+            }
+        }
+
+        private void ClearMessageTextBox()
+        {
+            messageTextBox.Text = "";
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+        }
+
+        private async void messageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                this.messageToSend = messageTextBox.Text;
+                messageTextBox.Text = "";
+                await DiscordChannel.SendMessage(messageToSend);
+                //await Task.Run(() =>
+                //{
+                //    e.Handled = true;
+                //    e.SuppressKeyPress = true;
+                //    DiscordChannel.SendMessage(messageTextBox.Text);
+                //    messageTextBox.Text = "";
+                //});
+            }
+        }
+
+        private void messageTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
