65a4a4fc40f44ae00ccd207755b56e9ac10403e7
Azeddine Bahri <63929044+AzeBah@users.noreply.github.com>
2020-06-11 11:46:05 -0400

    Add files via upload

--- DiscordChannel.cs
+++ DiscordChannel.cs
@@ -112,6 +112,10 @@ namespace WindowsFormsApp1
         // request to send a specific message to a channel
         public static void SendMessage(string message)
         {
+            if (channelId.Length != 18)
+            {
+                return;
+            }
             HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create("https://discord.com/api/v6/channels/" + channelId + "/messages");
             req.Method = "POST";
             req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36";
